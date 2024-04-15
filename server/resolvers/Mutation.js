37420ca42c4d82644bbe4c0c785838390c52cf9e
ikun <0215.miyatto@gmail.com>
2024-04-16 05:23:42 +0900

    updateJobとdeleteJobの修正

--- server/resolvers/Mutation.js
+++ server/resolvers/Mutation.js
@@ -1,10 +1,10 @@
 import { createJob, deleteJob, updateJob } from '../db/jobs.js';
-import { unAuthorizeError } from './Error.js';
+import { notFoundError, unAuthorizeError } from './Error.js';
 
 export const Mutation = {
   createJob: (parent, args, context) => {
     if (!context.user) {
-      throw unAuthorizeError("Missing authentication");
+      throw unAuthorizeError("認証されていません");
     };
     return createJob({
       companyId: context.user.companyId,
@@ -12,15 +12,30 @@ export const Mutation = {
       description: args.input.description,
     });
   },
-  deleteJob: (parent, args, context) => {
+  deleteJob: async (parent, args, context) => {
     const jobId = args.id;
-    return deleteJob(jobId);
+    if (!context.user) {
+      throw unAuthorizeError('認証されていません');
+    };
+    const job = await deleteJob(jobId, context.user.companyId);
+    if (!job) {
+      throw new notFoundError('jobが見つかりません');
+    };
+    return job;
   },
   updateJob: (parent, args, context) => {
-    return updateJob({
+    if (!context.user) {
+      throw unAuthorizeError('認証されていません');
+    }
+    const job = updateJob({
       id: args.input.id,
+      companyId: context.user.companyId,
       title: args.input.title,
       description: args.input.description,
     });
+    if (!job) {
+      throw notFoundError('jobが見つかりません');
+    };
+    return job;
   },
 };
